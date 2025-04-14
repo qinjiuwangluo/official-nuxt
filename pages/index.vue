d6355c3f42bc457fd86d669ee4699fbec701df71
Phoenix <1141988085@qq.com>
2025-04-14 13:38:30 +0800

    1

--- pages/index.vue
+++ pages/index.vue
@@ -14,9 +14,9 @@ const name = ref('comon')
 const data = ref({})
 const mock = await useMock()
 onMounted(async () => {
-  const dataObj =  await dataFetch(mock, window.location.host.split('.')[1])
-  name.value =dataObj.name
-  data.value =dataObj.data
+  const dataObj = await dataFetch(mock, window.location.host.split('.')[1])
+  name.value = dataObj.name
+  data.value = dataObj.data
 })
 const router = useRouter()
 const openPage = (path) => {
@@ -112,23 +112,12 @@ const openApp = (type) => {
         />
       </div>
     </div>
-    <svg
-      class="absolute left-0 bottom-[-1px] fill-white wave z-20"
-      fill=""
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 1000 300"
-     preserveAspectRatio="xMidYMid slice"
-    >
-      <path
-        d="M 1000 299 l 2 -279 c -155 -36 -310 135 -415 164 c -102.64 28.35 -149 -32 -232 -31 c -80 1 -142 53 -229 80 c -65.54 20.34 -101 15 -126 11.61 v 54.39 z"
-      ></path>
-      <path
-        d="M 1000 286 l 2 -252 c -157 -43 -302 144 -405 178 c -101.11 33.38 -159 -47 -242 -46 c -80 1 -145.09 54.07 -229 87 c -65.21 25.59 -104.07 16.72 -126 10.61 v 22.39 z"
-      ></path>
-      <path
-        d="M 1000 300 l 1 -230.29 c -217 -12.71 -300.47 129.15 -404 156.29 c -103 27 -174 -30 -257 -29 c -80 1 -130.09 37.07 -214 70 c -61.23 24 -108 15.61 -126 10.61 v 22.39 z"
-      ></path>
-    </svg>
+    <img
+      class="absolute left-0 right-0 w-full bottom-[-1px] z-20"
+      src="/assets/headwer_bg.png"
+      alt=""
+      srcset=""
+    />
   </div>
   <div id="about" class="py-120 w-full flex flex-wrap justify-center relative">
     <div
